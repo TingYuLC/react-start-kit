4ae3d2181cedf24503877b02975ac33baeec7203
little.luo <1317697565@qq.com>
2019-11-06 14:26:12 +0800

    add husky

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,10 +5,11 @@ module.exports = {
   rules: { 
     // 这里自定义规则
     // 可参考官方：https://stylelint.docschina.org/user-guide/rules/
+    // 属性排序规则: https://github.com/constverum/stylelint-config-rational-order
     "order/properties-order": [],
     "plugin/rational-order": [true, {
       "border-in-box-model": true,
       "empty-line-between-groups": false,
     }]
   }
- };
+};
